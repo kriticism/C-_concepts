251e086af4444ab364c6d27c6bec1e9f952792b2
Kriti Singh <kriti96.singh@gmail.com>
2017-07-08 02:18:50 +0530

    implement, code cleanup

--- 1-template_geneic functions.cpp
+++ 1-template_geneic functions.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
+using namespace std;
+/*
 
-// class treeNode{
-// public:
-//   int value;
-//   treeNode* next;
-//   treeNode(int, treeNode*);
-// }
-//
-// treeNode::treeNode (int N, treeNode* NEXT = NULL) {
-//   value = N;
-//   next = NEXT;
-// }
+ + used to create generic functions and classes
+ + type of data on which function/class operates is passed as parameter
 
-template <class X>
-void swapArgs(X &a, X &b){
-  X temp;
+ + used as:
+ template <class Ttype> returnType functionName(parameters){
+    // body of function
+  }
+
+*/
+
+// 1. function with one generic type
+template <class ValueType>
+void swapArgs(ValueType &a, ValueType &b){
+  ValueType temp;
   temp = a;
   a = b;
   b = temp;
 }
+
+// 2. function with two generic types
+template <class type1, class type2>
+void squarer(type1 a, type2 b){
+  cout<< a*a <<" "<< b*b <<endl;
+}
+
+// 3. new syntax for overloaded specialization of generic function to specific for char
+template<> void swapArgs<char>(char &a, char &b){
+  cout<< "[[[...char specialization of swap!...]]]";
+  char temp;
+  temp = a;
+  a = b;
+  b = temp;
+}
+
+
 int main(){
+
+  // 1. test for one generic type
   int i = 10, j = 20;
   cout<<i<<","<<j<<"->";
     swapArgs(i, j);
@@ -28,12 +48,21 @@ int main(){
   double d = 10.14, e = 20.02;
   cout<<d<<","<<e<<"->";
     swapArgs(d, e);
-  cout<<i<<","<<j<<endl;
+  cout<<d<<","<<e<<endl;
 
-  char b = '*', c = 'k';
+
+
+
+  // 2. test for two generic types
+  squarer(2.22, -22);
+
+  // 3. test for overloaded specialization of generic function
+
+  char b = 'x', c = '*';
   cout<<b<<","<<c<<"->";
     swapArgs(b, c);
   cout<<b<<","<<c<<endl;
+
 
   return 0;
 }
